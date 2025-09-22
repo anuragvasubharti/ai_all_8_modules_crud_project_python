b4fee7d484e21b14c4c5ae33820a17172faeaeeb
anuragvasubharti <anuragvasubharti@gmail.com>
2025-09-22 21:23:37 +0530

    Added

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Text
+from .database import Base
+
+class AIModel(Base):
+    __tablename__ = "ai_models"
+
+    id = Column(Integer, primary_key=True, index=True)
+    model_type = Column(String, index=True)
+    name = Column(String)
+    description = Column(Text)
+    result = Column(Text)
